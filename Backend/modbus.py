6742ad0b2b2d593264aa8eed50a03b6301bc1a2c
StratoPi <tatreu@ttu.ee>
2022-02-23 14:01:16 +0200

    Modbus

--- Backend/modbus.py
+++ Backend/modbus.py
@@ -5,27 +5,34 @@ import serial
 import modbus_tk
 import modbus_tk.defines as cst
 from modbus_tk import modbus_rtu
+#import params as ps
 
 PORT = '/dev/ttyAMA0'
 
 class Controller:
-    def __init__(self, operation, motor_number, reg_aadress, control_code)
-        self.operation=operation
+    def __init__(self, motor_number, reg_aadress, control_code):
         self.motor_number=motor_number
         self.reg_aadress=reg_aadress
         self.control_code=control_code
-
-        self.master = modbus_rtu.RtuMaster(
+        
+        logger = modbus_tk.utils.create_logger("console")
+        master = modbus_rtu.RtuMaster(
             serial.Serial(port='/dev/ttyAMA0', baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0)
         )
         master.set_timeout(5.0)
         master.set_verbose(True)
         logger.info("connected")
-    
-    def read_value(self):
 
-    def write_value(self):
-s = Controller
+        self.master=master
+        self.logger=logger
+
+    def read_value(self):
+        v = self.master.execute(self.motor_number, cst.READ_HOLDING_REGISTERS, self.reg_aadress, self.control_code)
+    #def write_value(self):
+    #    logger.info(master.execute(1, cst.WRITE_SINGLE_REGISTER, 8501, output_value=0x0006))
+
+motor_1 = Controller(1, 3207, 1)
+motor_1.read_value()
 
 def control():
     """main"""
@@ -87,4 +94,4 @@ def control():
     except modbus_tk.modbus.ModbusError as exc:
         logger.error("%s- Code=%d", exc, exc.get_exception_code())
 
-control()
+#control()
---
+++ Backend/params.py
@@ -0,0 +1,3 @@
+# This file contains motor control parameters
+
+READ = 
