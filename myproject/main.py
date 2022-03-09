8d828b0809def842b9e4571e8ab93764706c34e4
StratoPi <tatreu@ttu.ee>
2022-03-09 15:03:38 +0200

    Motor parameters

--- myproject/main.py
+++ myproject/main.py
@@ -8,3 +8,7 @@ print(f"Temperature: {tempstat()}°C")
 # Control motors and view their status
 #a = control(1, READ, MAINS_VOLTAGE, 1)[0]
 print(control(1, READ, MAINS_VOLTAGE, 1)[0])
+print(f"Current: {control(1, READ, MOTOR_CURRENT, 1)}\n\
+Frequency: {control(1, READ, OUTPUT_FREQ, 1)}\n\
+RPM: {control(1, READ, OUTPUT_VEL, 1)}\n\
+Power: {control(1, READ, MOTOR_POWER, 1)}")
