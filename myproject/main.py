52403430d9b8b23f8c259cdae8b98352891aa7b1
StratoPi <tatreu@ttu.ee>
2022-03-09 15:33:50 +0200

    Terminal controls

--- myproject/main.py
+++ myproject/main.py
@@ -7,8 +7,17 @@ print(f"Temperature: {tempstat()}°C")
 
 # Control motors and view their status
 #a = control(1, READ, MAINS_VOLTAGE, 1)[0]
-print(control(1, READ, MAINS_VOLTAGE, 1)[0])
-print(f"Current: {control(1, READ, MOTOR_CURRENT, 1)}\n\
-Frequency: {control(1, READ, OUTPUT_FREQ, 1)}\n\
-RPM: {control(1, READ, OUTPUT_VEL, 1)}\n\
-Power: {control(1, READ, MOTOR_POWER, 1)}")
+print(f"Voltage: {control(1, READ, MAINS_VOLTAGE, 1)[0]/10.0} V\n\
+Current: {control(1, READ, MOTOR_CURRENT, 1)[0]/10.0} A\n\
+Frequency: {control(1, READ, OUTPUT_FREQ, 1)[0]/10.0} Hz\n\
+RPM: {control(1, READ, OUTPUT_VEL, 1)[0]} rpm\n\
+Power: {control(1, READ, MOTOR_POWER, 1)[0]} %")
+
+#control mains
+mode = control(1, READ, REF_SWITCH, 1)[0] #if write then control 1 Read REF_SWITCH terminal/modbusis
+#modes = dict()
+print(f"Mode: {mode}")
+if mode == W_TERMINAL:
+    print("Terminal")
+if mode == W_MODBUS:
+    print("Modbus")
